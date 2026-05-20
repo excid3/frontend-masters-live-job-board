9e09d7d79c666edd02644aff5663a7f395771f8f
Chris Oliver <excid3@gmail.com>
2026-05-20 09:20:30 -0500

    Update mailer test

--- test/mailers/subscriber_mailer_test.rb
+++ test/mailers/subscriber_mailer_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class SubscriberMailerTest < ActionMailer::TestCase
   test "weekly_summary" do
-    mail = SubscriberMailer.weekly_summary
-    assert_equal "Weekly summary", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    mail = SubscriberMailer.with(subscriber: subscribers(:one)).weekly_summary
+    assert_equal "New jobs this week", mail.subject
+    assert_equal [ "one@example.com" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Unsubscribe", mail.body.encoded
   end
 end
